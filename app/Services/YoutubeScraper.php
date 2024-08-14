74903cd3885177b5dcfd6aad75f9fb088db9ced1
NekoFluff <alexnou@gmail.com>
2024-08-13 20:13:55 -0700

    chore: log html

--- app/Services/YoutubeScraper.php
+++ app/Services/YoutubeScraper.php
@@ -2,21 +2,27 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\BrowserKit\HttpBrowser;
+use Symfony\Component\HttpClient\HttpClient;
 
 class YoutubeScraper
 {
     public static function getVideoMetadata($url)
     {
-        $client = new HttpBrowser();
+        $browser = new HttpBrowser(HttpClient::create());
 
-        $crawler = $client->request('GET', $url);
+        $crawler = $browser->request('GET', $url);
 
         $title = $crawler->filter('meta[name="title"]')->attr('content', null);
         $description = $crawler->filter('meta[name="description"]')->attr('content', null);
         $html = $crawler->html();
         preg_match('/"scheduledStartTime":"(\d+)"/', $html, $matches);
         $livestream_start_dt = $matches[1] ?? null;
+
+        Log::info("HTML", [
+            'html' => $html,
+        ]);
 
         return [
             'title' => $title,
