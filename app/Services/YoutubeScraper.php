2b8e35bf9f351f9361b29b5d4bbd778cd5b26ddd
NekoFluff <alexnou@gmail.com>
2024-08-13 20:53:33 -0700

    fix: change header location

--- app/Services/YoutubeScraper.php
+++ app/Services/YoutubeScraper.php
@@ -12,7 +12,7 @@ class YoutubeScraper
 {
     public static function getVideoMetadata($url)
     {
-        $browser = new HttpBrowser(HttpClient::create([
+        $browser = new HttpBrowser(HttpClient::create()->withOptions([
             'headers' => [
                 'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
                 'Accept-Language' => 'en-US,en;q=0.9',
