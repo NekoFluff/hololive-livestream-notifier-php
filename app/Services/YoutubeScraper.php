8dc2f01b31e5ec65e0a82e660e064aa2c0a66beb
NekoFluff <alexnou@gmail.com>
2024-08-13 20:49:33 -0700

    fix: add Accept header

--- app/Services/YoutubeScraper.php
+++ app/Services/YoutubeScraper.php
@@ -16,6 +16,7 @@ class YoutubeScraper
             'headers' => [
                 'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
                 'Accept-Language' => 'en-US,en;q=0.9',
+                'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
             ],
         ]));
         $browser->request('GET', $url);
