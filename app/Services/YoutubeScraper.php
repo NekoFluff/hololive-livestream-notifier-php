f768d3e6d36e548d7decb832d965aa6b824a7174
NekoFluff <alexnou@gmail.com>
2024-08-13 20:34:44 -0700

    fix: try alternative web scraping

--- app/Services/YoutubeScraper.php
+++ app/Services/YoutubeScraper.php
@@ -2,18 +2,23 @@
 
 namespace App\Services;
 
+use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Log;
 use Symfony\Component\BrowserKit\HttpBrowser;
+use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\HttpClient\HttpClient;
 
 class YoutubeScraper
 {
     public static function getVideoMetadata($url)
     {
-        $browser = new HttpBrowser(HttpClient::create());
+        // $browser = new HttpBrowser(HttpClient::create());
+        // $browser->request('GET', $url);
+        // $crawler = $browser->getCrawler();
 
-        $browser->request('GET', $url);
-        $crawler = $browser->getCrawler();
+        $client = new Client();
+        $response = $client->request('GET', $url);
+        $crawler = new Crawler($response->getBody()->getContents());
 
         $title = $crawler->filter('meta[name="title"]')->attr('content', null);
         $description = $crawler->filter('meta[name="description"]')->attr('content', null);
